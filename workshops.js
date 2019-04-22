263fbf4b9a67a9d1e2dcff5e26a4e0658195a70c
Gabor Szabo <gabor@szabgab.com>
2019-04-22 11:00:04 +0300

    add link

--- workshops.js
+++ workshops.js
@@ -1,5 +1,13 @@
 function init() {
-    alert("hello");
+    var h1s = getElementsByName('h1');
+    console.log(h1s.length);
+    if (h1s.length != 1) {
+        console.error("Number of h1 tags is: " + h1s.length);
+    } else {
+        var title = h1s[0].innerHTML;
+        console.log(title);
+        h1s[0].innerHTML = '<a href="/">' + title + '</a>';
+    }
 }
 
 init();
