8e1896a386f46572b0448d58dcfd50accbbb2398
Gabor Szabo <gabor@szabgab.com>
2019-04-23 13:03:27 +0300

    script to create half-sized banner as well to display on our web page

--- create_banner.py
+++ create_banner.py
@@ -8,7 +8,8 @@ if (len(sys.argv) < 2):
 
 
 in_file = 'images/code-maven-workshops-1600x900.png'  # 1600, 900
-out_file = 'new.png'
+full_file = 'full.png'
+half_file = 'half.png'
 text = sys.argv[1]
 
 img = Image.open(in_file)
@@ -22,6 +23,11 @@ draw.polygon([(1600, 550), (1600, 900), (600, 900) ], fill = (255, 255, 255))
 font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 70)
 draw.text((1240, 670), 'Hosted by', (0, 0, 0), font=font)
 draw.text((930, 790), text, (0, 0, 0), font=font)
-img.save(out_file)
+img.save(full_file)
+print(full_file + ' created')
 
-print(out_file + ' created')
+
+size = (img.size[0]/2,  img.size[1]/2)
+img.thumbnail(size)
+img.save(half_file)
+print(half_file + ' created')
