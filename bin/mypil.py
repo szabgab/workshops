82d08d136a782a6356c29c206351a483d51b6c91
Gabor Szabo <gabor@szabgab.com>
2020-06-09 18:09:50 +0300

    experimenting with buttons for  youtube

--- bin/mypil.py
+++ bin/mypil.py
@@ -7,7 +7,7 @@ def heb(txt):
     rev = txt[::-1]
     return rev
 
-def add_text(draw, text, xy, size, rtl=False):
+def add_text(draw, text, xy, size, rtl=False, fill=(0, 0, 0)):
     font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', size)
     my_text = heb(text) if rtl else text
     width, height = font.getsize(my_text)
@@ -15,30 +15,30 @@ def add_text(draw, text, xy, size, rtl=False):
     draw.text(
         text = my_text,
         xy   = new_xy,
-        fill = (0, 0, 0),
+        fill = fill,
         font = font,
     )
 
-def add_date(date_right, date_top, font, draw, date, day, hours):
+def add_date(date_right, date_top, font, draw, date, day, hours, fill=(0, 0, 0)):
     date_width, date_height = font.getsize(date)
     draw.text(
         text=date,
         xy=(date_right-date_width, date_top),
-        fill=(0, 0, 0),
+        fill=fill,
         font=font,
     )
     day_width, day_height = font.getsize(day)
     draw.text(
         text=day,
         xy=(date_right-day_width, date_top+day_height+10),
-        fill=(0, 0, 0),
+        fill=fill,
         font=font,
     )
     hours_width, hours_height = font.getsize(hours)
     draw.text(
         text=hours,
         xy=(date_right-hours_width, date_top + day_height + hours_height + 25),
-        fill=(0, 0, 0),
+        fill=fill,
         font=font,
     )
 
---
+++ bin/youtube-button-hebrew.py
@@ -0,0 +1,67 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import heb, add_text, add_date
+
+width  = 350
+height = 350
+
+# background_color = '#eb8634'
+# background_color = '#ebb434'
+background_color = '#FFFFFF'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    # Code-Maven Workshop
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+
+    add_text(
+        draw = draw,
+        text = "מביני קוד",
+        rtl  = True,
+        xy   = (330, 70),
+        size = 70,
+        fill = (0, 0, 255),
+    )
+
+    add_text(
+        draw = draw,
+        text = "עקוב אחרי",
+        rtl  = True,
+        xy   = (340, 170),
+        size = 70,
+        fill = (0, 0, 255),
+    )
+
+#    add_text(
+#        draw = draw,
+#        text = "איך משתמשים בסלאק?",
+#        rtl  = True,
+#        xy   = (1200, 130),
+#        size = 60,
+#    )
+
+
+    isize = 350
+    #embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
+    #embed_image(img=img, filename='python.png', box=(10, 10), mask=True)
+    #embed_image(img=img, filename='LI-In-Bug.png', size=(300, 300), box=(10, 10), mask=True)
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
