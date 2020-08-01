a495fcfff794e070328a48cf22dad9f39b54f45f
Gabor Szabo <gabor@szabgab.com>
2020-08-01 16:32:33 +0300

    go thumbnail

---
+++ bin/thumbnail-go.py
@@ -0,0 +1,77 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import add_text, add_date, embed_image
+
+width  = 1280
+height = 720
+
+# background_color = '#eb8634'
+#background_color = '#ebb434'
+background_color = '#DDDDDD'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+    add_text(
+        draw = draw,
+        text = "Getting Started",
+        rtl  = False,
+        size = 80,
+        xy   = (250, 30),
+    )
+
+    add_text(
+        draw = draw,
+        text = "with the Golang",
+        rtl  = False,
+        size = 80,
+        xy   = (250, 240),
+    )
+
+    add_text(
+        draw = draw,
+        text = "Course",
+        rtl  = False,
+        size = 80,
+        xy   = (250, 440),
+    )
+
+    #add_text(
+    #    draw = draw,
+    #    text = "in less than",
+    #    rtl  = False,
+    #    size = 60,
+    #    xy   = (250, 440),
+    #)
+
+    #add_text(
+    #    draw = draw,
+    #    text = "14 minutes",
+    #    rtl  = False,
+    #    size = 60,
+    #    xy   = (250, 540),
+    #)
+
+
+    isize = 350
+    embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
+    embed_image(img=img, filename='golang.png', box=(10, 10), mask=True)
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
