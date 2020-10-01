7404868366f7dd3fd29e3645dd7c33327bddbb10
Gabor Szabo <gabor@szabgab.com>
2020-10-01 08:49:06 +0300

    thumbnail

---
+++ bin/thumbnail-hebrew-git-for-github-users.py
@@ -0,0 +1,77 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import heb, add_text, add_date
+
+width  = 1280
+height = 720
+
+# background_color = '#eb8634'
+# background_color = '#ebb434'
+background_color = '#EEEEEE'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def embed_image(img, filename, box, size=None, mask=False):
+    emb_img_path = os.path.join(root, 'src', filename)
+    emb_img = Image.open(emb_img_path, 'r')
+    #size = (emb_img.size[0] / 2, emb_img.size[1] / 2)
+    if size:
+        emb_img.thumbnail(size)
+    if mask:
+       img.paste(emb_img, box=box, mask=emb_img)
+    else:
+       img.paste(emb_img, box=box)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    # Code-Maven Workshop
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+
+    add_text(
+        draw = draw,
+        text = "Git for GitHub users",
+        rtl  = False,
+        xy   = (400, 80),
+        size = 70,
+    )
+
+
+    add_text(
+        draw = draw,
+        text = "גיט למשתמשי גיטהאב",
+        rtl  = True,
+        xy   = (800, 350),
+        size = 70,
+    )
+
+#    add_text(
+#        draw = draw,
+#        text = "איך משתמשים בסלאק?",
+#        rtl  = True,
+#        xy   = (1200, 130),
+#        size = 60,
+#    )
+
+
+    isize = 350
+    embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
+    embed_image(img=img, filename='digital-ocean-icon-blue.png', box=(30, 500), mask=True)
+    embed_image(img=img, filename='Octocat.png', size=(300, 300), box=(10, 10), mask=True)
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
