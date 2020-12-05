9d8c4510b3eba687be2616ceb240b9dd456948fe
Gabor Szabo <gabor@szabgab.com>
2020-12-05 09:46:35 +0200

    up

---
+++ bin/thumbnail-rt-cpan-is-closing.py
@@ -0,0 +1,74 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import add_text, add_date, embed_image
+
+width  = 1280
+height = 720
+
+# background_color = '#eb8634'
+#background_color = '#ebb434'
+background_color = '#DDDDDD'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    # Code-Maven Workshop
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+
+    add_text(
+        draw = draw,
+        text = "RT.CPAN.ORG is closing",
+        rtl  = False,
+        size = 80,
+        xy   = (150, 170),
+    )
+
+    add_text(
+        draw = draw,
+        text = "Update your",
+        rtl  = False,
+        size = 80,
+        xy   = (230, 320),
+    )
+
+    add_text(
+        draw = draw,
+        text = "META",
+        rtl  = False,
+        size = 80,
+        xy   = (230, 450),
+    )
+
+
+    add_text(
+        draw = draw,
+        text = "data",
+        rtl  = False,
+        size = 80,
+        xy   = (230, 580),
+    )
+
+
+    isize = 350
+    embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
+    embed_image(img=img, filename='cpan.png', box=(10, 10), mask=False)
+    embed_image(img=img, filename='metacpan.jpg', box=(10, 410), mask=False)
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
