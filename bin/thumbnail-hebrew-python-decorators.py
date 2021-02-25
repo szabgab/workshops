75784994da3ebc7939fbdf6e884818d0b9309ff7
Gabor Szabo <gabor@szabgab.com>
2021-02-25 10:50:01 +0200

    add

---
+++ bin/thumbnail-hebrew-python-decorators.py
@@ -0,0 +1,75 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import heb, add_text, add_date
+
+width  = 1280
+height = 720
+
+# background_color = '#eb8634'
+# background_color = '#ebb434'
+background_color = '#EEEEEE'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def embed_image(img, filename, box, size=None, mask=False):
+    emb_img_path = os.path.join(root, 'src', filename)
+    emb_img = Image.open(emb_img_path, 'r')
+    #size = (emb_img.size[0] / 2, emb_img.size[1] / 2)
+    if size:
+        emb_img.thumbnail(size)
+    if mask:
+       img.paste(emb_img, box=box, mask=emb_img)
+    else:
+       img.paste(emb_img, box=box)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    # Code-Maven Workshop
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+
+    add_text(
+        draw = draw,
+        text = "דקורטורים בפייתון",
+        rtl  = True,
+        xy   = (1000, 100),
+        size = 70,
+    )
+
+    add_text(
+        draw = draw,
+        text = "Decorators in Python",
+        rtl  = False,
+        xy   = (100, 470),
+        size = 70,
+    )
+
+
+#    add_text(
+#        draw = draw,
+#        text = "תכנות בפייתון עם מונגו",
+#        rtl  = True,
+#        xy   = (850, 350),
+#        size = 70,
+#    )
+
+    isize = 350
+    embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
+    embed_image(img=img, filename='python.png', size=(300, 300), box=(10, 10), mask=True)
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
