31fe5d965f98b6f8fa5f515c952f17cd12f35c62
Gabor Szabo <gabor@szabgab.com>
2021-04-11 09:16:58 +0300

    rex

--- bin/thumbnail-rex.py
+++ bin/thumbnail-rex.py
@@ -35,7 +35,7 @@ def main():
 
     add_text(
         draw = draw,
-        text = "(R)?ex - part 2",
+        text = "(R)?ex - part 3",
         rtl  = False,
         size = 80,
         xy   = (200, 200),
