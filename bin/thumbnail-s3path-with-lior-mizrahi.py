eead1e551a208b0973e45e379fedad0a5c6a4f4b
Gabor Szabo <gabor@szabgab.com>
2021-05-04 06:52:50 +0300

    update

--- bin/thumbnail-s3path-with-lior-mizrahi.py
+++ bin/thumbnail-s3path-with-lior-mizrahi.py
@@ -27,32 +27,57 @@ def main():
     draw = ImageDraw.Draw(img)
     add_text(
         draw = draw,
-        text = "Live web development",
+        text = "Python s3path, the pathlib",
         rtl  = False,
         size = 80,
-        xy   = (350, 30),
+        xy   = (200, 30),
     )
 
     add_text(
         draw = draw,
-        text = "part 5",
+        text = "extension for AWS S3",
         rtl  = False,
         size = 80,
-        xy   = (350, 200),
+        xy   = (200, 200),
     )
 
 
     add_text(
         draw = draw,
-        text = "Mark Gardner          Gábor Szabó",
+        text = "Lior Mizrahi              Gábor Szabó",
         rtl  = False,
         size = 80,
         xy   = (10, 420),
     )
+#
+#    add_text(
+#        draw = draw,
+#        text = "and Gábor Szabó",
+#        rtl  = False,
+#        size = 80,
+#        xy   = (50, 540),
+#    )
 
-    embed_image(img=img, filename='mark_gardner.jpeg',   size=(200, 200), box=(10, 500))
+    #add_text(
+    #    draw = draw,
+    #    text = "in less than",
+    #    rtl  = False,
+    #    size = 60,
+    #    xy   = (250, 440),
+    #)
+
+    #add_text(
+    #    draw = draw,
+    #    text = "14 minutes",
+    #    rtl  = False,
+    #    size = 60,
+    #    xy   = (250, 540),
+    #)
+
+
+    embed_image(img=img, filename='lior_mizrahi.jpeg',   size=(200, 200), box=(10, 500))
     embed_image(img=img, filename='gabor2_612x612.jpg', size=(200, 200), box=(width-200-10, 500))
-    embed_image(img=img, filename='mojolicious.png', box=(10, 10), mask=True)
+    embed_image(img=img, filename='python.png', box=(10, 10), mask=True)
 
     img.save(png_filepath)
     img.show()
