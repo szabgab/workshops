942e8c42a3da47cc67338d18af90e4b29f26125b
Gabor Szabo <gabor@szabgab.com>
2022-09-29 08:47:33 +0300

    new

---
+++ bin/thumbnail-docker-1.py
@@ -0,0 +1,60 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import add_text, add_date, embed_image
+
+width  = 1280
+height = 720
+
+background_color = '#eb8634'
+#background_color = '#ebb434'
+#background_color = '#DDDDDD'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+    add_text(
+        draw = draw,
+        text = "Docker Course - 1",
+        rtl  = False,
+        size = 80,
+        xy   = (250, 30),
+    )
+
+    add_text(
+        draw = draw,
+        text = "Introduction",
+        rtl  = False,
+        size = 80,
+        xy   = (250, 200),
+    )
+
+
+    #add_text(
+    #    draw = draw,
+    #    text = "               Gábor Szabó",
+    #    rtl  = False,
+    #    size = 80,
+    #    xy   = (10, 420),
+    #)
+
+    embed_image(img=img, filename='gabor2_612x612.jpg', size=(200, 200), box=(width-200-10, 500))
+    embed_image(img=img, filename='docker-moby-logo.png', size=(200, 200), box=(10, 10), mask=True)
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
