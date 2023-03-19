404df8d0b1ab2a753486bde20f858b3473e5baa8
Gabor Szabo <gabor@szabgab.com>
2023-03-19 19:27:25 +0200

    add

--- bin/osdc-2023-01-public-hebrew.py
+++ bin/osdc-2023-01-public-hebrew.py
@@ -24,8 +24,8 @@ def main():
     else:
         end = 1
 
-    # for episode in ['1', '2-1', '2-2', '3-1', '3-2', '4-1', '4-2', '5-1', '5-2', '6-1', '6-2', '7-1', '7-2']: #range(1, end+1):
-    for episode in ['8-1', '8-2']: #range(1, end+1):
+    # for episode in ['1', '2-1', '2-2', '3-1', '3-2', '4-1', '4-2', '5-1', '5-2', '6-1', '6-2', '7-1', '7-2', '8-1', '8-2']: #range(1, end+1):
+    for episode in ['9-1', '9-2']: #range(1, end+1):
         create_thumbnail(episode=episode, show=(end==1))
 
 
