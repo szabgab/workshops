7db3e23627be82cb7ec28c2b9552194048c71747
Gabor Szabo <gabor@szabgab.com>
2023-07-19 10:07:25 +0300

    thumbnail

---
+++ bin/thumbnail-he-open-source-anyone-can-change-but-central-control.py
@@ -0,0 +1,73 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from mypil import add_text, add_date, embed_image, heb
+
+width  = 1280
+height = 720
+
+#background_color = '#eb8634'
+#background_color = '#ebb434'
+background_color = '#DDDDDD'
+
+root = os.path.dirname(os.path.dirname(__file__))
+#print(root)
+
+def main():
+    png_filename = os.path.splitext(os.path.basename(__file__))[0] + '.png'
+    #print(png_filename)
+
+    png_filepath = os.path.join(root, 'images', png_filename)
+    #print(png_filepath)
+
+    img = Image.new('RGB', (width, height), color=background_color)
+
+    # Code-Maven Workshop
+
+    font_gabor = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
+    font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
+    draw = ImageDraw.Draw(img)
+    add_text(
+        draw = draw,
+        text = heb("אם כל אחד יכול לשנות קוד פתוח"),
+        rtl  = True,
+        size = 65,
+        xy   = (1250, 30),
+    )
+
+    add_text(
+        draw = draw,
+        text = heb("אז איך אפשר לסמוך על זה?"),
+        rtl  = True,
+        size = 65,
+        xy   = (1250, 180),
+    )
+
+    add_text(
+        draw = draw,
+        text = "If anyone can change open source code",
+        rtl  = False,
+        size = 64,
+        xy   = (20, 420),
+    )
+
+    add_text(
+        draw = draw,
+        text = "then how can we trust it?",
+        rtl  = False,
+        size = 65,
+        xy   = (20, 580),
+    )
+
+
+    isize = 350
+    #embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
+    #embed_image(img=img, filename='flask.png', size=(isize, isize), box=(10, 10), mask=True)
+    #embed_image(img=img, filename='code_maven_440x440.png', size=(isize, isize), box=(width-isize-10, 360), mask=True)
+
+
+    img.save(png_filepath)
+    img.show()
+
+
+main()
