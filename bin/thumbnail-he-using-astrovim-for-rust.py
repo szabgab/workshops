76526b7213e323df465e7e75908cb8f81232eb3e
Gabor Szabo <gabor@szabgab.com>
2024-04-24 12:46:02 +0300

    add

--- bin/thumbnail-he-using-astrovim-for-rust.py
+++ bin/thumbnail-he-using-astrovim-for-rust.py
@@ -5,9 +5,11 @@ from mypil import add_text, add_date, embed_image, heb
 width  = 1280
 height = 720
 
-background_color = '#eb8634'
-#background_color = '#ebb434'
+#background_color = '#eb8634'
+background_color = '#ebb434'
 # background_color = '#DDDDDD'
+#background_color = "#59D5E0"
+#background_color = "#3ea3c1"
 
 root = os.path.dirname(os.path.dirname(__file__))
 #print(root)
@@ -27,50 +29,44 @@ def main():
     font_title = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 160)
     font = ImageFont.truetype('Pillow/Tests/fonts/FreeSansBold.ttf', 40)
     draw = ImageDraw.Draw(img)
-
     add_text(
         draw = draw,
-        text = "Python Programming course",
-        rtl  = False,
-        size = 80,
-        xy   = (50, 50),
+        text = heb("שימוש באסטרו ווים לפיתוח ראסט"),
+        rtl  = True,
+        size = 65,
+        xy   = (1250, 30),
     )
 
     add_text(
         draw = draw,
-        text = "Weizmann Institute - 2-3 (part 6)",
-        rtl  = False,
-        size = 80,
-        xy   = (50, 150),
-    )
-
-
-    add_text(
-        draw = draw,
-#        text = "About GitHub, Programming",
-#        text = "GitHub Pages via GitHub UI",
-#        text = "Installing git on Windows",
-#        text = "Installing VS Code and Python",
-#        text = "Getting input from STDIN",
-        text = "flow control in Python",
-        rtl  = False,
-        size = 80,
-        xy   = (50, 420),
+        text = heb("עם מאיר קריחלי"),
+        rtl  = True,
+        size = 65,
+        xy   = (1250, 180),
     )
 
     add_text(
         draw = draw,
-        text = "if, while, and break",
+        text = "Using AstroNvim for Rust development",
         rtl  = False,
-        size = 95,
-        xy   = (50, 540),
+        size = 64,
+        xy   = (20, 420),
+    )
+
+    add_text(
+        draw = draw,
+        text = "With Meir Kriheli",
+        rtl  = False,
+        size = 65,
+        xy   = (20, 580),
     )
 
 
     isize = 350
     #embed_image(img=img, filename='gabor2_612x612.jpg', size=(isize, isize), box=(width-isize-10, height-isize-10))
-    #embed_image(img=img, filename='rust-logo-512x512.png', size=(isize, isize), box=(10, 10), mask=True)
+    #embed_image(img=img, filename='vim-322x318.png', size=(isize, isize), box=(10, 10), mask=True)
     #embed_image(img=img, filename='code_maven_440x440.png', size=(isize, isize), box=(width-isize-10, 360), mask=True)
+
 
 
     img.save(png_filepath)
